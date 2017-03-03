5d8b844cb0e5f9dbf4474b894366f6d629f97fbe
busster <jasonmbuss@gmail.com>
2017-03-03 13:12:46 -0600

    ASDF

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Skycast
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    class Application < Rails::Application
+      config.middleware.use Rack::Deflater
+    end
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do

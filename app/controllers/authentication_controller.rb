b1e0f2a56a831ff4c13f12245db39d2a6bdc26c8
busster <jasonmbuss@gmail.com>
2017-03-05 16:28:14 -0600

    ASDf

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -17,8 +17,12 @@ class AuthenticationController < ApplicationController
 
   def authenticate_user
     user = User.find_for_database_authentication(email: params[:email])
-    if user.valid_password?(params[:password])
-      render json: payload(user)
+    if user
+      if user.valid_password?(params[:password])
+        render json: payload(user)
+      else
+        render json: {errors: ['Invalid Username/Password']}, status: :unauthorized
+      end
     else
       render json: {errors: ['Invalid Username/Password']}, status: :unauthorized
     end

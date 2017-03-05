ffedaf8fdb0a35ebf8bebc5e6a08110dabb947c2
busster <jasonmbuss@gmail.com>
2017-03-04 20:57:17 -0600

    ASdf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,25 +2,12 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   # before_filter :authenticate_user!
   protect_from_forgery with: :null_session
-  before_filter :cors_preflight_check
+
 
   def index
   end
 
 
-
-
-
-
-def cors_preflight_check
-  if request.method == :options
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-    headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    render :text => '', :content_type => 'text/plain'
-  end
-end
 
 
 
